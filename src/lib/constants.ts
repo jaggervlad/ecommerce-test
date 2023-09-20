5670f2cc7c95f40384ce942d6863547613eb16c3
Sebastian Acosta Alonso <45018754+jaggervlad@users.noreply.github.com>
2023-09-20 11:32:04 -0500

    fixed production url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 
 export const BASE_URL_API = IS_PRODUCTION
-  ? 'https://ecommerce-test-ruddy.vercel.app/'
+  ? 'https://ecommerce-test-ruddy.vercel.app'
   : 'http://localhost:3000/api';
